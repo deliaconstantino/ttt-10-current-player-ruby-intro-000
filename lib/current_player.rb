745513bc699fb109bafd9a2e3fdf8b4207cd8def
Delia Constantino <dconstan92@gmail.com>
2020-10-13 14:52:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,4 @@
 
-#puts board.to_s
-
 def current_player(board)
   turns_played = turn_count(board)
   if turns_played % 2 == 0
@@ -11,22 +9,17 @@ def current_player(board)
 end
 
 def turn_count(board)
-  num_turns = 0
-  board.each do |space|
-    if "#{space}" == "X" || "#{space}" == "O"
-      num_turns += 1
-    end
-  end
-  return num_turns
-end
 
 
-# board.each do |count|
-#   puts "#{count}"
-#   play = gets.chomp
-#   occupied_positions = 0
-#   if play == "X" || play == "O"
-#     occupied_positions += 1
+# def turn_count(board)
+#   num_turns = 0
+#   board.each do |space|
+#     if "#{space}" == "X" || "#{space}" == "O"
+#       num_turns += 1
+#     end
 #   end
+#   return num_turns
 # end
-# return occupied_positions
+#
+
+#
