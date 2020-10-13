1bebec55d5d76fd029246f3c8577e3c4e2c784a8
Delia Constantino <dconstan92@gmail.com>
2020-10-13 14:15:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,28 +1,15 @@
 board = Array.new(9, " ")
 #puts board.to_s
 
-# def turn_count(board)
-#   count = 0
-#   num_turns = 0
-#   while count < 9
-#   puts "#{board[count]}"
-#   play = gets.chomp
-#   if play == "X" || play == "O"
-#     num_turns + 1
-#   end
-#     count += 1
-# end
-# end
-
-#turn_count = board.each do |count|
-  puts "#{count}"
-  play = gets.chomp
-  num_turns = 0
-  if play == "X" || play == "O"
-    num_turn += 1
-  end
-  return num_turns
-end
-
 def current_play(board)
 end
+
+# board.each do |count|
+#   puts "#{count}"
+#   play = gets.chomp
+#   num_turns = 0
+#   if play == "X" || play == "O"
+#     num_turn += 1
+#   end
+#   return num_turns
+# end
