cd28dba0e9973d2c2f7f3cbfcfc2161eb73aab3a
Delia Constantino <dconstan92@gmail.com>
2020-10-13 15:19:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -17,30 +17,3 @@ def turn_count(board)
   end
   return num_turns
 end
-
-
-# def turn_count(board)
-#   count = 0
-#   num_turns = 0
-#   while count < 9
-#     play = "#{board[count]}"
-#     if play == "X" || play == "O"
-#       num_turns += 1
-#     end
-#     count += 1
-#   end
-#   return num_turns
-# end
-
-# def turn_count(board)
-#   num_turns = 0
-#   board.each do |space|
-#     if "#{space}" == "X" || "#{space}" == "O"
-#       num_turns += 1
-#     end
-#   end
-#   return num_turns
-# end
-#
-
-#
