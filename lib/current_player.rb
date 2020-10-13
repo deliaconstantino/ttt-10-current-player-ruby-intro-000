bf58edcf8a5b6c01b1b972d10447622911a6a7d8
Delia Constantino <dconstan92@gmail.com>
2020-10-13 14:01:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,5 +2,14 @@ board = Array.new(9, " ")
 #puts board.to_s
 
 def turn_count(board)
-  board.each do |turn|
-    
+  count = 0
+  num_turns = 0
+  while count < board[count]
+  puts "#{board[count]}"
+  play = gets.strip
+  if play == "X" || play == "O"
+    num_turns + 1
+  end
+    count += 1
+end
+end
