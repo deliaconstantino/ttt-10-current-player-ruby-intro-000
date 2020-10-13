ced72bc4b9bf17aa03c1167799ee4098e6f17fcf
Delia Constantino <dconstan92@gmail.com>
2020-10-13 14:21:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,10 @@ board = Array.new(9, " ")
 #puts board.to_s
 
 def current_play(board)
+  if num_turn % 2 == 0
+    current_player = "X"
+  elsif num_turn % 2 == 1
+    current_player = "O'"
 end
 
 def turn_count(board)
